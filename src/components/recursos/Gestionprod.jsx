a212860be689f891c965e2be88c01b1247e59050
cristhianmart <martinezcuestacris@gmail.com>
2023-05-16 19:22:20 -0500

    se agrega codigo para que se muestre la barra de modificaciones de album

--- src/components/recursos/Gestionprod.jsx
+++ src/components/recursos/Gestionprod.jsx
@@ -1,22 +1,41 @@
-import React from "react";
-import Nav from "../helpers/Nav";
+import React from 'react'
+import Nav from '../helpers/Nav'
 
 const Gestionprod = () => {
-   return (
-      <section className="panel-container">
-         <Nav />
-         <section className="panel">
-            <section className="panel-conf">
-               <button className="btnadmi">Modificar Album</button>
-               <button className="btnadmi">Borrar Album</button>
-               <button className="btnadmi">Ocultar Album</button>
-            </section>
-            <section className="panel-vpalbum">
-               <h1>Vista previa de los albumnes</h1>
-            </section>
-         </section>
-      </section>
-   );
-};
+    const modificar = document.querySelector('#modificar')
+    function mostrarmodif() {
+        modificar.classList.add('vista-modificar2')
+        modificar.classList.remove('vista-modificar')
+    }
 
-export default Gestionprod;
+    return (
+        <section className="panel-container">
+            <Nav />
+            <section className="panel">
+                <section className="panel-conf">
+                    <button className="btnadmi" onClick={mostrarmodif}>
+                        Modificar Album
+                    </button>
+                    <button className="btnadmi">Borrar Album</button>
+                    <button className="btnadmi">Ocultar Album</button>
+                </section>
+                <section className="vista-modificar" id="modificar">
+                    <label className="nombre">Nombre de producción</label>
+
+                    <input className="texto" type="text" placeholder="" />
+                    <div>*vista previa imagenes*</div>
+                    <textarea
+                        className="texto"
+                        cols="30"
+                        rows="10"
+                        placeholder="Agregar descripción de la producción fotografica"></textarea>
+                </section>
+                <section className="panel-vpalbum">
+                    <h1>Vista previa de los albumnes</h1>
+                </section>
+            </section>
+        </section>
+    )
+}
+
+export default Gestionprod
