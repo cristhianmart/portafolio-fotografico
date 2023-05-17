a79e4c8373c9198ad0d1fe19a374561fc613040f
laurafernandezv <laurafernandezgotadeleche@gmail.com>
2023-05-16 19:19:07 -0500

    se modifico info de las produciones con sus respectivos css y tambien la bio con s u css basico

--- src/components/recursos/Produccion5.jsx
+++ src/components/recursos/Produccion5.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-import Header from "../helpers/Header"
+import Header from './Header'
+import casalujo from '../assets/img/casa-lujo.jpg'
 
 const Produccion5 = () => {
-   return (
-      <section>
-         <Header />
-      </section>
-   );
-};
+  return (
+    <section>
+        <Header/>
+        <h1 className='tituloproduccion'>CASAS DE LUJO</h1>
+    <section className='produccion'>
+      <div>
+      <img src={casalujo} alt="" className='imgProduccion'/> 
+      </div>
+      <div className='textoProduccion'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat optio doloribus fugit earum vitae totam id, deserunt distinctio? Earum vel quibusdam a odio assumenda illum sapiente laudantium laborum iusto.
+      At, facilis ullam cumque quidem corrupti esse iste nisi consectetur ut sapiente molestias ab dolorum? Dicta harum ducimus enim deserunt maiores ut ipsam. Voluptatem nihil fugiat, asperiores alias rem quis.
+      Aperiam debitis veniam optio, quis fugit culpa! Explicabo eos corporis, perspiciatis incidunt distinctio aliquam neque pariatur cupiditate officiis optio rerum consequuntur libero veniam, velit a numquam aut amet ea laboriosam?
+      Beatae suscipit laborum, doloremque, alias culpa fuga eum ab ea provident atque accusamus odit eius tempore nostrum ipsa quia libero eveniet harum, adipisci non unde nobis? Ducimus magni eius laboriosam?
+      E quo id vero enim consequatur! Quisquam tempore soluta cumque reprehenderit eaque veritatis voluptatibus officia nam? Illum doloremque laboriosam, culpa corporis unde quaerat.</p>
+        </div>
+        </section>
+       <div className='sectionProduccion'>
+       <img src={casalujo} alt="" className='imagenes'/>
+        <img src={casalujo} alt="" className='imagenes'/>
+        <img src={casalujo} alt="" className='imagenes'/>
+       </div>
 
-export default Produccion5;
+        <div className='sectionProduccion'>
+        <img src={casalujo} alt="" className='imagenes'/>
+        <img src={casalujo} alt="" className='imagenes'/>
+      
+        </div>
+    </section>
+  )
+}
+
+export default Produccion5
